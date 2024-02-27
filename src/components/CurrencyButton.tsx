f270f710e1878daba29cb42d5a9e1a67849fa3fc
julio889570 <julio889570@gmail.com>
2024-02-26 22:09:31 -0800

    first

---
+++ src/components/CurrencyButton.tsx
@@ -0,0 +1,37 @@
+import type { PropsWithChildren } from 'react'
+import React from 'react'
+
+import {View, Text, StyleSheet} from 'react-native'
+
+type CurrencyButtonProps = PropsWithChildren<{
+    name: string;
+    flag: string;
+}>
+
+const CurrencyButton = (props: CurrencyButtonProps): JSX.Element =>{
+    return(
+        <View style={styles.ButtonContainer}>
+                <Text style={styles.flag}>{props.flag}</Text>
+                <Text style={styles.country}>{props.name}</Text>
+        </View>
+    )
+}
+
+export default CurrencyButton
+
+const styles = StyleSheet.create({
+    ButtonContainer: {
+        alignItems: 'center'
+    },
+    flag:{
+        fontSize: 28,
+        color: '#ffffff',
+        marginBottom: 4,
+    },
+    country:{
+        fontSize: 14,
+        color: '#2d3436',
+        
+    }
+
+})
